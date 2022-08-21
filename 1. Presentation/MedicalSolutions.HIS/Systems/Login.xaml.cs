8b1b5e357cb0ee2af8535012756228afbefc208b
DuyNV <duynguyen.19x@gmail.com>
2022-08-21 16:24:50 +0700

    Commit

--- 1. Presentation/MedicalSolutions.HIS/Systems/Login.xaml.cs
+++ 1. Presentation/MedicalSolutions.HIS/Systems/Login.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MedicalSolutions.IProviders.HIS.Dictionary;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MedicalSolutions.HIS
+namespace MedicalSolutions.HIS.Systems
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-
-            //IUserProvider instance = Activator.CreateInstance<IUserProvider>();
         }
     }
 }
